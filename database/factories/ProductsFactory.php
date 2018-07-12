afe83b679e1989176e0d6021b92351154bf2b63d
Nikita240 <Rush3nik@gmail.com>
2018-07-12 15:19:44 -0700

    Add cost to products

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->company()
+        'name' => $faker->company(),
+        'cost' => $faker->numberBetween(1, 10000)
     ];
 });
